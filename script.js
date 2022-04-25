dc3e17a293aaeafc95a433fdb580ac5829795fad
Jéssica <jessicaguedes.ads@gmail.com>
2022-04-25 17:03:18 -0400

    limpar campo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function tabuada(){
   let saida = document.getElementById('saida')
   let n = Number(document.getElementById('fnum').value)
+  let campo = document.getElementById('fnum')
 
   saida.innerHTML = `<h2> Tabuada de ${n}</h2><br>`
   let c = 1
@@ -9,4 +10,6 @@ function tabuada(){
     c ++ 
     }
     saida.style.textAlign = "center"
+    campo.value = '';
+    campo.focus()
 }
